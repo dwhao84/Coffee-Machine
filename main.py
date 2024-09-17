0c493fe4976885866d2a3bd67e81dd9fcd4a0a4b
Dawei84 <dwhao84@gmail.com>
2024-09-17 20:04:26 +0800

    Create the menu.py, and create small function for calculation.

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from menu import MENU
+
+print(MENU)
 
 resources = {
     "water": 300,
@@ -41,16 +19,41 @@ resources = {
 # Nickel:  $0.05
 # Penny:   $0.01
 
+# 判斷要產生哪一種功能:
+def check_input(input):
+    if input == "expresso":
+        print("Expresso")
+    elif input == "latte":
+        print("latte")
+    elif input == "cappuccino":
+        print("cappuccino")
+    elif input == "result":
+        print("Result")
+    else:
+        print("Wrong entering")
+
+# 計算價錢
+def calculate(quarter, dime, nickel, penny):
+    total = (quarter * 0.25) + (dime * 0.1) + (nickel * 0.05) + (penny * 0.01)
+    print(f"USD $ { total }")
+    return total
+
 # Requirements
 # TODO: 1. Print report
-# TODO: 2. Check resources sufficient? 確認資源是否足夠
-# TODO: 3. Process coin
-# TODO: 4. Check transaction successful?
-# TODO: 5. Make coffee.
+result = input("What would you like? ( Expresso / Latte / Cappuccino): ").lower()
 
-result = input("What would you like? ( Expresso / Latte / Cappuccino): ")
+# TODO: 2. Check resources sufficient? 確認資源是否足夠
+check_input(result)
+
+# TODO: 3. Process coin
 print("Please insert coins.")
-print("How many quarters?")
-print("How many dime?")
-print("How many nickel?")
-print("How many penny?")
+quarter = int(input("How many quarters ? $ "))
+dime    = int(input("How many dime ? $ "))
+nickel  = int(input("How many nickel ? $ "))
+penny   = int(input("How many penny ? $ "))
+calculate(quarter=quarter, dime=dime, nickel=nickel, penny=penny)
+
+# TODO: 4. Check transaction successful?
+
+
+# TODO: 5. Make coffee.
