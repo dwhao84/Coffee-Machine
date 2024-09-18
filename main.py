658b8d622530ab3814d4ed1bcab4d61918826dc5
Dawei Hao <dwsamurai84@gmail.com>
2024-09-18 12:05:05 +0800

    Create Requirements.md. Adjust the functions and refactor the code.

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ from menu import MENU
 # Dime:    $0.1
 # Nickel:  $0.05
 # Penny:   $0.01
-
-# print(MENU)
-
 resources = {
     "water": 300,
     "milk": 200,
@@ -20,11 +17,13 @@ resources = {
     "money": 50
 }
 
+should_continue = True
 current_money = resources["money"]
 selected_drinks_cost = 0
+inserted_coin = 0
 user_selection = ""
 
-# 顯示目前庫存數:
+# TODO: 1. Print report
 def show_report():
     water  = resources["water"]
     milk   = resources["milk"]
@@ -32,71 +31,100 @@ def show_report():
     money  = resources["money"]
     print(f"Water:{ water }ml \nmilk: { milk }ml \ncoffee: { coffee }g \nmoney: $ {money}")
 
-# 判斷要產生哪一種功能:
-def check_input(entered_prompt):
-    if entered_prompt == "expresso":
-        print(MENU["espresso"]["ingredients"])
-        selected_drinks_cost = MENU["espresso"]["cost"]
-        print(selected_drinks_cost)
+# TODO: 2. Check resources sufficient? 確認資源是否足夠
+def entered_prompt(prompts):
+    if MENU[prompts]["ingredients"]["water"] > resources["water"]:
+        print("Sorry there is not enough water.")
+    elif MENU[prompts]["ingredients"]["milk"] > resources["milk"]:
+        print("Sorry there is not enough milk.")
+    elif MENU[prompts]["ingredients"]["coffee"] > resources["coffee"]:
+        print("Sorry there is not enough coffee.")
+    else:
+        print("We have enough resources, We're ready to go.")
 
-    elif entered_prompt == "latte":
-        print(MENU["latte"]["ingredients"])
-        selected_drinks_cost = MENU["latte"]["cost"]
-        print(selected_drinks_cost)
+# Make sure the leftover resources.
+def calculated_leftover(prompts):
+    leftover_water = resources["water"] - MENU[prompts]["ingredients"]["water"]
+    leftover_milk = resources["milk"] - MENU[prompts]["ingredients"]["milk"]
+    leftover_coffee = resources["coffee"] - MENU[prompts]["ingredients"]["coffee"]
+    income         = resources["money"] + MENU[prompts]["cost"]
+    print(f"Water:{ leftover_water }ml \nmilk: { leftover_milk }ml \ncoffee: { leftover_coffee }g \ncurrent bank account: $ {income}")
 
-    elif entered_prompt == "cappuccino":
-        print(MENU["cappuccino"]["ingredients"])
-        selected_drinks_cost = MENU["cappuccino"]["cost"]
-        print(selected_drinks_cost)
+# TODO: 3. Process coin
+def insert_coins():
+    print("Please insert coins.")
+    quarter = int(input("How many quarters ? $ "))
+    dime    = int(input("How many dime ? $ "))
+    nickel  = int(input("How many nickel ? $ "))
+    penny   = int(input("How many penny ? $ "))
+    inserted_coins = (quarter * 0.25) + (dime * 0.1) + (nickel * 0.05) + (penny * 0.01)
+    print(f"USD $ { inserted_coins }")
+    return inserted_coins
 
-    elif entered_prompt == "report":
+# TODO: 4. Check transaction successful?
+def transaction_checking(inserted_coin):
+    if inserted_coin > selected_drinks_cost:
+        print("We've enough money.")
+    elif inserted_coin == selected_drinks_cost:
+        print("I found just the right amount of money. ")
+    else:
+        print("Will make your coffee later.")
+
+# ========================================================================================================
+
+while should_continue:
+    # Requirements
+    prompt = input("What would you like? ( Expresso / Latte / Cappuccino): ").lower()
+
+    # 判斷要產生哪一種功能:
+    if prompt == "expresso":
+        selected_drinks_cost = MENU[prompt]["cost"]
+        print(f"Drink cost $ { selected_drinks_cost }")
+        entered_prompt(prompts=prompt)
+        inserted_coin = insert_coins()
+        
+        user_selection = prompt
+        
+        transaction_checking(inserted_coin=inserted_coin)
+        
+        # TODO: 5. Make coffee.
+        calculated_leftover(prompts=prompt)  # 包含selected_drinks_cost
+        print(f"Here is your {user_selection}. Enjoy!")
+        
+    elif prompt == "latte":
+        selected_drinks_cost = MENU[prompt]["cost"]
+        print(f"Drink cost $ { selected_drinks_cost }")
+        entered_prompt(prompts=prompt)
+        inserted_coin = insert_coins()
+        user_selection = prompt
+        
+        transaction_checking(inserted_coin=inserted_coin)
+        
+        # TODO: 5. Make coffee.
+        calculated_leftover(prompts=prompt)  # 包含selected_drinks_cost
+        print(f"Here is your {user_selection}. Enjoy!")
+    
+    elif prompt == "cappuccino":
+        selected_drinks_cost = MENU[prompt]["cost"]
+        print(f"Drink cost $ { selected_drinks_cost }")
+        entered_prompt(prompts=prompt)
+        inserted_coin = insert_coins()
+        
+        user_selection = prompt
+        transaction_checking(inserted_coin=inserted_coin)
+        
+        # TODO: 5. Make coffee.
+        calculated_leftover(prompts=prompt)  # 包含selected_drinks_cost
+        print(f"Here is your {user_selection}. Enjoy!")
+        
+    elif prompt == "report":
         show_report()
-
-    elif entered_prompt == "off":
+    
+    elif prompt == "off":
+        should_continue = False
         print("Coffee Machine is turning off.")
 
     else:
+        should_continue = False
         print("Wrong entering")
 
-
-
-# 計算價錢
-def calculate(quarters, dimes, nickels, pennys, drinks_cost):
-    total = (quarters * 0.25) + (dimes * 0.1) + (nickels * 0.05) + (pennys * 0.01)
-    print(f"USD $ { total }")
-    print(drinks_cost)
-
-    if current_money > total and total >= drinks_cost:
-        print("Is enough money")
-        print(current_money, total, drinks_cost)
-
-    elif current_money == total and total == drinks_cost:
-        print("Just enough")
-        print(current_money, total, drinks_cost)
-
-    else:
-        print("Sorry that's not enough money. Money refunded.")
-
-
-# Requirements
-# TODO: 1. Print report
-prompt = input("What would you like? ( Expresso / Latte / Cappuccino): ").lower()
-user_selection = prompt
-check_input(prompt)
-
-# TODO: 2. Check resources sufficient? 確認資源是否足夠
-
-
-
-# TODO: 3. Process coin
-print("Please insert coins.")
-quarter = int(input("How many quarters ? $ "))
-dime    = int(input("How many dime ? $ "))
-nickel  = int(input("How many nickel ? $ "))
-penny   = int(input("How many penny ? $ "))
-calculate(quarters=quarter, dimes=dime, nickels=nickel, pennys=penny, drinks_cost=selected_drinks_cost)
-
-# TODO: 4. Check transaction successful?
-
-# TODO: 5. Make coffee.
-print(f"Here is your { user_selection }. Enjoy!")
